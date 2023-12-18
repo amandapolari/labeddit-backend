52218f7c909371ea009ab8d5bb8f569d86cbadbe
Amanda <amandaoliveirapolari@gmail.com>
2023-12-18 16:57:32 -0300

    [test] tested endpoints: createPost

---
+++ tests/business/posts/createPost.test.ts
@@ -0,0 +1,52 @@
+import { PostBusiness } from '../../../src/business/PostBusiness';
+import { CreatePostSchema } from '../../../src/dtos/posts/createPostDto';
+import { CommentDatabaseMock } from '../../mocks/database/CommentDatabaseMock';
+import { PostDatabaseMock } from '../../mocks/database/PostDatabaseMock';
+import { UserDatabaseMock } from '../../mocks/database/UserDatabaseMock';
+import { IdGeneratorMock } from '../../mocks/services/IdGeneratorMock';
+import { TokenManagerMock } from '../../mocks/services/TokenManagerMock';
+import messages from '../../../src/messages/messages.json';
+import { BadRequestError } from '../../../src/errors/BadRequestError';
+
+describe('testing createPost business', () => {
+    const postBusiness = new PostBusiness(
+        new IdGeneratorMock(),
+        new TokenManagerMock(),
+        new PostDatabaseMock(),
+        new UserDatabaseMock(),
+        new CommentDatabaseMock()
+    );
+
+    test('deve retornar uma mensagem de sucesso ao criar um post', async () => {
+        const input = CreatePostSchema.parse({
+            token: 'token-mock-amanda',
+            content: 'Post mock',
+        });
+
+        const returned = await postBusiness.createPost(input);
+
+        const expected = {
+            message: messages.post_created_sucess,
+            content: 'Post mock',
+        };
+
+        expect(returned).toEqual(expected);
+    });
+
+    test('deve disparar erro se o token for inválido', async () => {
+        expect.assertions(2);
+
+        try {
+            const input = CreatePostSchema.parse({
+                token: 'abc',
+                content: 'Post mock',
+            });
+            await postBusiness.createPost(input);
+        } catch (error) {
+            if (error instanceof BadRequestError) {
+                expect(error.message).toBe(messages.invalid_token);
+                expect(error.statusCode).toBe(400);
+            }
+        }
+    });
+});
