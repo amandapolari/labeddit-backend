636f70adef47376d19e80641d3b79c95374d486f
amandapolari <amandaoliveirapolari@gmail.com>
2024-01-19 11:23:38 -0400

    [fix] fix .env example path and ping test to test connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+// import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { userRouter } from './router/userRouter';
 import dotenv from 'dotenv';
@@ -16,6 +17,25 @@ const port = Number(process.env.PORT) || 3003;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
+
+// test ping
+// app.get('/ping', async (req: Request, res: Response) => {
+//     try {
+//         res.status(200).send({ message: 'Pong!' });
+//     } catch (error) {
+//         console.log(error);
+
+//         if (req.statusCode === 200) {
+//             res.status(500);
+//         }
+
+//         if (error instanceof Error) {
+//             res.send(error.message);
+//         } else {
+//             res.send('Erro inesperado');
+//         }
+//     }
+// });
 
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
